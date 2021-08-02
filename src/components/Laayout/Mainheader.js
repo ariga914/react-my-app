fb4623b8ff245d75e9b050cc3cfc555ddbc84335
ariga914 <82427651+ariga914@users.noreply.github.com>
2021-08-02 11:31:32 +0900

    init

---
+++ src/components/Laayout/Mainheader.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Navigation from "./Navigation";
+
+const Mainheader = () => {
+    return (
+        <Fragment>
+            <h1>Manage Allowance</h1>
+            <Navigation />
+        </Fragment>
+    );
+};
+
+export default Mainheader;
